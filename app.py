fb3cca3d05f9729a33ba833b780a623a630c4b2c
Naveen <naveen@shorthills.ai>
2026-06-12 04:07:28 +0000

    test1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from try-deployment CI/CD 🚀"
+    return "Hello from try-deployment"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
